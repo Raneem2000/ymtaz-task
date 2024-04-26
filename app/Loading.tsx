a3acfaadf23437f8a84ef165a1ca9ecb33e5c877
Raneem2000 <rdasouki6@gmail.com>
2024-04-27 00:25:01 +0300

    add skelton

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,8 +1,30 @@
 const Loading = () => {
+  const skelton = Array.from({ length: 3 });
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-    </div>
+    <>
+      {skelton.map((_, index) => (
+        <div
+          key={index}
+          className="bg-backGround mx-10 p-2 my-8 rounded shadow"
+        >
+          <div className="">
+            <div className="flex justify-between items-center gap-1">
+              <div className="flex flex-col gap-2 w-44">
+                <span className="bg-gray h-4 w-24 rounded animate-pulse" />{" "}
+                <span className="bg-gray h-4 w-16 rounded animate-pulse" />{" "}
+              </div>
+              <div className="flex flex-col gap-2 justify-end items-end">
+                <span className="bg-gray h-4 w-20 rounded animate-pulse" />{" "}
+                <span className="bg-gray h-4 w-32 rounded animate-pulse" />{" "}
+              </div>
+            </div>
+            <button className="mt-4 p-2 rounded-xl bg-gray-200 w-full animate-pulse">
+              &nbsp;
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
